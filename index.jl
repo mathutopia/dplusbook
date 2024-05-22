3b58862ecffa28714a3fb3746049deee2cc5ba10
mathutopia <mathutopia@163.com>
2024-05-22 21:49:25 +0800

    fix yaml

--- index.jl
+++ index.jl
@@ -38,7 +38,7 @@ Box = "~1.0.13"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.10.2"
+julia_version = "1.10.3"
 manifest_format = "2.0"
 project_hash = "68906de5f775ed1434647dc50131d3c40655f579"
 
@@ -47,9 +47,9 @@ uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
 
 [[deps.Box]]
 deps = ["HypertextLiteral", "Markdown"]
-git-tree-sha1 = "bee6dbf5fa690f991d4c3b018cbfbb206e59dc18"
+git-tree-sha1 = "c43838e1b85ae396b37551d2b48f71efdb1d4bbe"
 uuid = "247ae7ab-d1b9-4f88-8529-b44b862cffa0"
-version = "1.0.14"
+version = "1.0.15"
 
 [[deps.HypertextLiteral]]
 deps = ["Tricks"]
@@ -69,7 +69,7 @@ version = "0.1.8"
 
 # ╔═╡ Cell order:
 # ╠═85cba6b0-1078-495b-9888-6943353b034c
-# ╟─c053c944-c600-4d6c-9b10-b3af15eb4392
+# ╠═c053c944-c600-4d6c-9b10-b3af15eb4392
 # ╟─6f509f57-9574-405a-9eaa-890249b603e3
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
